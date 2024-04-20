571f4dafc5330927400fa11997e5643ed01b36c1
Joyce Beatriz <48606497+joycebeatriz@users.noreply.github.com>
2024-04-20 02:04:57 -0300

    Adição da classe Cidade

--- endereco/src/Cidade.java
+++ endereco/src/Cidade.java
@@ -1,3 +1,27 @@
 public class Cidade {
-    
+    private String nome;
+
+    // Construtor para inicializar o nome da cidade
+    public Cidade(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter para o nome da cidade
+    public String getNome() {
+        return nome;
+    }
+
+    // Setter para o nome da cidade
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Método toString para representar o objeto Cidade como uma String
+    @Override
+    public String toString() {
+        return "Cidade{" +
+               "nome='" + nome + '\'' +
+               '}';
+    }
 }
+
--- endereco/src/Main.java
+++ endereco/src/Main.java
@@ -2,8 +2,12 @@ public class Main {
     public static void main(String[] args) {
         // Criando uma instância da classe Bairro
         Bairro bairro = new Bairro("Bela Vista");
-
         // Imprimindo o nome do bairro
         System.out.println("Nome do Bairro: " + bairro.getNome());
+
+        // Criando uma instância da classe Cidade
+        Cidade cidade = new Cidade("São Paulo");
+        // Imprimindo o nome da cidade
+        System.out.println("Nome da Cidade: " + cidade.getNome());
     }
 }
